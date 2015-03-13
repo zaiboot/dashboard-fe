c5a405cb1e83c2c4c356751704ba95490a824e4e
Veronica Elizondo <veronica.elizondo@trintech.com>
2015-03-13 08:45:31 -0600

    Adding reconciliation statistics datasource

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,122 +1,109 @@
 //dashboard.js
-(function($) {
-	
-	var config = {
-		
-		Categories: [ "Not Started", "Pending", "Pending Modification", "Waiting on Review", 
-			"Review Pending Recertification", "Waiting on Approval", "Approval Pending Recertification",
-			"Complete"	
-		],
-		Series: [			
-			{
-				name: "Due Prior",
-				data: [ 
-					100, 
-					150, 
-					200, 
-					250, 
-					300, 
-					350, 
-					400, 
-					450
-				]
-			},
-			{
-				name: "Today",
-				data: [ 
-					90, 
-					190, 
-					45, 
-					521, 
-					300, 
-					350, 
-					444, 
-					500
-				]
-			},
-			{
-				name: "This week",
-				data: [ 
-					190, 
-					140, 
-					451, 
-					51, 
-					301, 
-					135, 
-					144, 
-					54
-				]
-			},
-			{
-				name: "After This week",
-				data: [ 
-					190, 
-					140, 
-					451, 
-					51, 
-					301, 
-					135, 
-					144, 
-					54
-				]
-			}
-		]
-	};
-	
-	var chartConfig =  {
-                title: {
-                    text: "Chart for"
-                },
-				 chartArea: {
-					background: "transparent"
-				  },
-                legend: {
-					labels: {
-						color: "#5d5d5d"
-					},
-					position: "bottom",
-					orientation: "horizontal",
-                    visible: true
-                },
-                seriesDefaults: {
-                    type: "bar",
-                  	stack: true,
-					
-                },
-				seriesClick: function(e) {					
-					console.log("Category = ", e.category, " Series.name", e.series.name);
-					
-				},
-				seriesColors: [ "#96172e", "#2e64a7", "#c0d0e4", "#e1e8f2"],
-                series: config.Series,
-                valueAxis: {
-                    max: 700,
-					color: "#5d5d5d",
-                    line: {
-                        visible: false
-                    },
-                    minorGridLines: {
-                        visible: true
-                    }
-                },
-                categoryAxis: {
-                    categories: config.Categories,
-					color: "#5d5d5d"
-                },
-                tooltip: {
-                    visible: true,
-                    template: "#= series.name #: #= value #"
+
+function createChart() {
+    var config = {
+
+        Categories: ["Not Started", "Pending", "Pending Modification", "Waiting on Review", "Review Pending Recertification",
+                        "Waiting on Approval", "Approval Pending Recertification", "Complete"],
+        Series: [
+            {
+                name: "Due Prior",
+                data: [100, 150, 200, 250, 300, 350, 400, 450]
+            },
+            {
+                name: "Today",
+                data: [90, 190, 45, 521, 300, 350, 444, 500]
+            },
+            {
+                name: "This week",
+                data: [190, 140, 451, 51, 301, 135, 144, 54]
+            },
+            {
+                name: "After This week",
+                data: [190, 140, 451, 51, 301, 135, 144, 54]
+            }
+        ]
+    };
+
+
+    $("#chart").kendoChart({
+        title: {
+            text: "Reconciliation Statistics"
+        },
+        chartArea: {
+            background: "transparent"
+
+        },
+        legend: {
+            labels: {
+                color: "#5d5d5d"
+            },
+            position: "bottom",
+            orientation: "horizontal",
+            visible: true
+        },
+        seriesDefaults: {
+            type: "bar",
+            stack: true
+        },
+        seriesClick: function(e) {
+            console.log("Category = ", e.category, " Series.name", e.series.name);
+
+        },
+        seriesColors: ["#96172e", "#2e64a7", "#c0d0e4", "#e1e8f2"],
+        //series: config.Series,
+        dataSource: {
+            transport: {
+                read: {
+                    url: "../data/statistics",
+                    dataType: "json"
                 }
-            };
-	
-          $(document).ready(function() {
-          	kendo.ui.progress($("#panelbar"), true);
-			$("#panelbar").kendoPanelBar({
-            	expandMode: "single"
-            });
-			$("#chart").kendoChart(chartConfig);
-			
-			
-            
-          });
-})($)
+            },
+            sort: {
+                field: "name",
+                dir: "asc"
+            }
+        },
+        valueAxis: {
+            max: 700,
+            color: "#5d5d5d",
+            line: {
+                visible: false
+            },
+            minorGridLines: {
+                visible: false
+            }
+        },
+        categoryAxis: {
+            categories: config.Categories,
+            color: "#5d5d5d",
+            majorGridLines: {
+                visible: false
+            }
+        },
+        tooltip: {
+            visible: false,
+            format: "{0}"
+        }
+    });
+}
+
+$(document).ready(function () {
+    createChart();
+    $(document).bind("kendo:skinChange", createChart);
+    $(".options").bind("change", refresh);
+});
+
+function refresh() {
+    var chart = $("#chart").data("kendoChart"),
+        series = chart.options.series,
+        type = $("input[name=seriesType]:checked").val(),
+        stack = $("#stack").prop("checked");
+
+    for (var i = 0, length = series.length; i < length; i++) {
+        series[i].stack = stack;
+        series[i].type = type;
+    };
+
+    chart.refresh();
+}
